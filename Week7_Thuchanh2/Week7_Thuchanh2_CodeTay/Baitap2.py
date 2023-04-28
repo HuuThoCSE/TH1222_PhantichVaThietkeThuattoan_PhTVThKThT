d18bcd99ace419a55862869ec8f4d91abd32b10c
Hữu Thọ CS <huutho.cs@gmail.com>
2023-04-28 08:49:30 +0700

    8h31am28-04-2023

--- Week7_Thuchanh2/Week7_Thuchanh2_CodeTay/Baitap2.py
+++ Week7_Thuchanh2/Week7_Thuchanh2_CodeTay/Baitap2.py
@@ -1,2 +1,18 @@
 class Danhsach():
-    def __int__(self):
+    def __init__(self, lst=None):
+        if lst is None:
+            self.data = []
+        else:
+            self.data = lst
+
+    def Nhap(self):
+        n = int(input("Nhập số lượng phần từ: "))
+        for i in range(n):
+            self.data.append(int(f"Nhập phần tử thứ {i+1} ="))
+
+    def Xuat(self):
+        print("Danh sách số nguyên", self.data)
+
+ds = Danhsach()
+ds.Nhap()
+ds.Xuat()
